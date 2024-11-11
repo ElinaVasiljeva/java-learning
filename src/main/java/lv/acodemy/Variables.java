d4f3e6b768404a16b908f5cc3599fb04e5d0273e
Elina.V <koshka19984@gmail.com>
2024-11-11 21:08:52 +0200

    [feature/TWP-1337]

---
+++ src/main/java/lv/acodemy/Variables.java
@@ -0,0 +1,113 @@
+package lv.acodemy;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        //this is my comment
+
+        /*
+        this is 1 line
+        this is 2 line
+         */
+
+        //int(integer) - whole number / natural number
+        int age = 30;
+        int currentYear = 2024;
+        // current_year = snake case - never do like this!!!
+        // current-year = kebab case - never do like this!!!
+        //currentYear = camel case (lower camel case) - good
+
+        int inventoryItemCount = 100;
+
+        System.out.println(age);
+        System.out.println(currentYear);
+        System.out.println(inventoryItemCount);
+
+        //double (fractional numbers)
+         double temperature = 36.6;
+         double price = 19.99;
+         double distance = 38400.0;
+
+
+        System.out.println(temperature);
+        System.out.println(price);
+        System.out.println(distance);
+
+        // char (symbols)
+
+        char grade = 'A';
+        char myInitial = 'N';
+        char currentSymbol = '$';
+
+        // Print them all;
+
+        System.out.println(grade);
+        System.out.println(myInitial);
+        System.out.println(currentSymbol);
+
+        //boolean (logical type, can keep only two values: true or false)
+        //how to name& has or is
+        //example: isSummer / if(isSummer) {}; / isLoggedIn;
+        // Example: hasAccess / if(hasAccess) {};
+
+        boolean hasAccess = true;
+        System.out.println(hasAccess);
+
+        boolean isSummer = false;
+        System.out.println(isSummer);
+
+
+        // long
+        long phoneNumber = 21431211;
+
+        // float
+        float interestRate = 4.5f;
+
+        // Arithmetic operators
+
+        int a = 10;
+        int b = 5;
+        int sum = a+b; // add
+        System.out.println(sum);
+
+        int c = 20;
+        int d = 8;
+        int difference = c - d;
+        System.out.println(difference);
+
+        int n = 7;
+        int m = 3;
+        int result = n * m;
+        System.out.println(result);
+
+        // Divide
+
+        int num1 = 10;
+        int num2 = 3;
+        int division = num1/num2;
+        System.out.println(division);
+
+        double num3 = 10.0;
+        double num4 = 3.0;
+        double divResult = num3 / num4;
+        System.out.println(divResult);
+
+        // % (reminder of a division)
+
+        int f = 12;
+        int g = 5;
+        int reminder = f % g;
+        System.out.println(reminder);
+
+        int number = 8;
+        boolean isEven = (number );
+
+
+
+
+
+
+
+
+    }
+}
